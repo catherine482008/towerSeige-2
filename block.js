5cd46a6b6311232936f04711f62abb460abbac00
catherine482008 <72607852+catherine482008@users.noreply.github.com>
2021-10-08 14:49:36 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,35 @@
+class Block{
+    constructor(x,y,width,height){
+        var options={
+            restitution:1.0,
+            friction:0.0
+        }
+        this.visibility=225;
+        this.body=Bodies.rectangle (x,y,width,height,options);
+        World.add (world,this.body);
+        this.width=width;
+        this.height=height;
+    }
+    display(){
+        
+        if(this.body.speed<3){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height)
+        pop()
+        }else {
+            World.remove(world,this.body)
+           
+            push()
+            this.visibility=this.visibility-5
+            tint(225,this.visibility)
+            pop()
+            
+        }
+    }
+}   
+  
